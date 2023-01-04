14d6ad75a3ffa39aa4ca579c28247bb09c00c23a
aslanovgx <aslanov7509@gmail.com>
2023-01-04 16:57:56 +0400

    #send section is completed

--- src/js/home.js
+++ src/js/home.js
@@ -1,16 +1,28 @@
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+const Images = document.querySelector('.back_image')
+// console.log(Images.style.backgroundImage);
+var time = 3000
+
+Images[0] = './../src/images/h3-slider-background.jpg'
+Images[1] = './../src/images/h3-slider-background-2.jpg'
+Images[2] = './../src/images/h3-slider-background-3.jpg'
+
+
+
+for (let i = 0; i < 3; i++) {
+    const element = Images[i];
+    function ChangeImg() {
+        Images.src = Images[i]
+        if (i < 2) {
+            Images.style.transform = 'scale(1.08)'
+            i++;
+        }
+        else {
+            Images.style.transform = 'scale(1.02)'
+            i = 0;
+        }
+
+
+        setTimeout('ChangeImg()', time);
+    }
+}
+window.onload = ChangeImg;
