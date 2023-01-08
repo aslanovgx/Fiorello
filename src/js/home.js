2cc44719a7b4e4f0c4334ea470c48892f51e6959
aslanovgx <aslanov7509@gmail.com>
2023-01-08 13:41:06 +0400

    #swiper_navigation and #footer sections are completed

--- src/js/home.js
+++ src/js/home.js
@@ -62,6 +62,12 @@ if (İframe_Btn) {
     })
 }
 
+var swiper = new Swiper(".swiper-navigate", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 
 
