a9d3c9ef085b4a186441b92111cb997024d2a6eb
aslanovgx <aslanov7509@gmail.com>
2023-01-09 16:46:32 +0400

    Product page is finished with responsivity

--- src/js/product.js
+++ src/js/product.js
@@ -37,3 +37,23 @@ if(Btn3) {
         Row_3.style.display = "block"
     })
 }
+
+
+// -------------------------------------------
+
+const Mobile_Nav = document.querySelector('.mobile_box')
+const X_Btn = document.querySelector('.fa-xmark')
+const Bar_nav = document.querySelector('.fa-bars')
+
+if(Bar_nav){
+    Bar_nav.addEventListener('click', ()=> {
+    Mobile_Nav.style.transform = "translateX(0px)"
+    // console.log('sa');
+    })
+}
+if(X_Btn){
+    X_Btn.addEventListener('click', ()=> {
+    Mobile_Nav.style.transform = "translateX(100%)"
+    // console.log('sa');
+    })
+}
