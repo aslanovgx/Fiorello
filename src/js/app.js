e1f5288a0cad8b6e2b5ca8b4f9a0b78c292fd01b
aslanovgx <aslanov7509@gmail.com>
2023-01-15 22:41:49 +0400

    some variations

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+var Home_Id = document.getElementById('home_id')
+
+Home_Id.addEventListener('click', () => {
+    console.log('sdsd');
+})
--- src/js/home.js
+++ src/js/home.js
@@ -58,11 +58,7 @@ if (İframe_Remove_Btn) {
 
     })
 }
-var Home_Id = document.getElementById('home_id')
 
-Home_Id.addEventListener('click', () => {
-    console.log('sdsd');
-})
 
 
 
