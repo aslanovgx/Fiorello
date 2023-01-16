eebea9f80b1652111df37d36a66022de8d37e4e8
aslanovgx <aslanov7509@gmail.com>
2023-01-16 19:33:48 +0400

    dsfgsffd

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,6 @@ var Home_Id = document.getElementById('home_id')
 
 if(Home_Id) {
     Home_Id.addEventListener('click', () => {
-        console.log('sdsd');
+        console.log('sdsaksjkhgsajsghfdd');
     })
 }
